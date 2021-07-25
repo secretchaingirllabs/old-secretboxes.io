1d3490b04c8df5c44298416ba205d770dbd45b0d
Roman Munar <roman.munar@chromedia.com>
2021-07-25 18:02:26 +0800

    fixed build error

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 const globby = require('globby')
 const prettier = require('prettier')
-const { SITE_URL } = require('../src/config/seo')
 
+const SITE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'https://localhost:3000'
+    : 'https://secretboxes.vercel.app'
+//
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
