29d127c0335745fbbd07598c074b38d4fbc682f5
Roman Munar <roman.munar@chromedia.com>
2021-07-25 15:59:53 +0800

    added hero section in homepage

---
+++ src/components/Pages/Home/HeroSection.tsx
@@ -0,0 +1,76 @@
+import { AlienWithBoxesLottie } from '@/components/Lotties/AlienWithBoxesLottie'
+
+export const HeroSection = () => (
+  <section>
+    <div className="max-w-[1152px] mx-auto">
+      <div className="relative flex items-center justify-between space-y-4">
+        <div className="pt-10 mx-auto md:mx-0 md:-mt-8 text-blue-primary">
+          <h1 className="leading-tight font-display text-[118px] dark:text-primary">
+            Secret Boxes
+          </h1>
+          <p className="mb-5 text-2xl font-body dark:text-dark-body">
+            The beautiful, fun and easy way to get
+            <br />
+            started building Secret Apps.
+          </p>
+          <div className="flex space-x-6">
+            <button
+              type="button"
+              className="flex items-center justify-center px-4 py-2 font-semibold text-white transition-colors rounded-lg group bg-gray-body hover:bg-gray-label"
+            >
+              Start Unboxing
+              <div className="transition-all opacity-0 group-hover:scale-100 group-hover:opacity-100 group-hover:ml-2">
+                <svg
+                  className="hidden group-hover:block"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.167 7.81718L3.16699 7.81718"
+                    stroke="#FCFCFC"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M9.13379 3.80089L13.1671 7.81689L9.13379 11.8336"
+                    stroke="#FCFCFC"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            </button>
+            <button
+              type="button"
+              className="px-6 py-3 font-semibold bg-white border rounded-lg border-gray-body text-gray-body"
+            >
+              Tutorials
+            </button>
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <AlienWithBoxesLottie
+            style={{ width: '636px', height: '498px', paddingTop: '30px' }}
+          />
+        </div>
+      </div>
+      <a href="#secretBoxes">
+        <svg
+          className="w-6 h-6 mx-auto mt-20 dark:text-dark-body"
+          width="18"
+          height="10"
+          viewBox="0 0 18 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 1L9 9L17 1" stroke="currentColor" strokeWidth="2" />
+        </svg>
+      </a>
+    </div>
+  </section>
+)
